6f6866902079a1a5ad2c5881d456c0f272560bee
Farah <farahhanyy766@gmail.com>
2026-02-13 01:41:13 +0200

    basics

---
+++ lib/screens/news_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class NewsScreen extends StatelessWidget {
+  static const String routeName = 'NewsScreen';
+  const NewsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
